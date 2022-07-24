e0429c1ee2651525e26ad203fba079ce82199d47
OJ287 <enhouri@gmail.com>
2022-07-24 17:17:15 +0900

    新規作成

---
+++ src/Part2/Test1.java
@@ -0,0 +1,18 @@
+package Part2;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] test1 = new String[2];
+		test1[0]="te";
+		test1[1]="st";
+		System.out.println(test1[0]);
+		
+
+		//ERROR: 后面指定具体的数据的时候，就不可指定配列的长度
+//		String[] test2 = new String[2] {"te","st"};
+		
+	}
+
+}
